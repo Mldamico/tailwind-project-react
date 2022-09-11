9fef183a49b2999a69f5f009beaf916f5b6294ec
mldamico <matias.lioneldamico@gmail.com>
2022-09-11 12:12:39 -0300

    Basic bookmark done

--- src/pages/bookmark/Bookmark.tsx
+++ src/pages/bookmark/Bookmark.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Download from "./components/Download";
 import DownloadBoxes from "./components/DownloadBoxes";
+import Faq from "./components/Faq";
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Nav from "./components/Nav";
 import Tabs from "./components/Tabs";
+import FaqAccordion from "./components/FaqAccordion";
+import Newsletter from "./components/Newsletter";
 
 export default function Bookmark() {
   return (
@@ -15,6 +18,9 @@ export default function Bookmark() {
       <Tabs />
       <Download />
       <DownloadBoxes />
+      <Faq />
+      <FaqAccordion />
+      <Newsletter />
     </>
   );
 }
